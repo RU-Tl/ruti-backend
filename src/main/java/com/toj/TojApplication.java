920b66c6cf820773a89ba35f1edd5ca71cf8859d
zzjiho <youngyoungnim@gmail.com>
2024-05-23 01:32:48 +0900

    Feat: 기초 엔티티 설계

--- src/main/java/com/toj/TojApplication.java
+++ src/main/java/com/toj/TojApplication.java
@@ -1,13 +1,23 @@
 package com.toj;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TojApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TojApplication.class, args);
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 
 }
