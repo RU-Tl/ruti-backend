980fa66e1357a64dd96646ed72da2ad00851f2bb
hwajin-jo <wa8784@naver.com>
2024-05-25 15:52:22 +0900

    Update : ãswagger ìer 스키마 작성

--- src/main/java/com/toj/dto/member/UserLoginDto.java
+++ src/main/java/com/toj/dto/member/UserLoginDto.java
@@ -1,10 +1,13 @@
 package com.toj.dto.member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UserLoginDto {
 
+    @Schema(description = "회원 이름", example = "안이연")
     private String name;
+    @Schema(description = "회원 이메일", example = "lxxyeon@gmail.com")
     private String email;
 }
--- src/main/java/com/toj/dto/routine/CreateRoutineResponse.java
+++ src/main/java/com/toj/dto/routine/CreateRoutineResponse.java
@@ -1,12 +1,12 @@
 package com.toj.dto.routine;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class CreateRoutineResponse {
-
+    @Schema(description = "루틴 id", example = "1")
     private Long routineId;
-
 }
